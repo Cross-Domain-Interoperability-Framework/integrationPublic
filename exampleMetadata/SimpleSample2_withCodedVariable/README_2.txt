00a18f1affad697fb49f260692320eb3300fba82
Stephen Richard <smrTucson@gmail.com>
2025-11-18 14:47:18 +0100

    sync to web

---
+++ exampleMetadata/SimpleSample2_withCodedVariable/README_2.txt
@@ -0,0 +1,4 @@
+This simple sample of DDI-CDI employs the CDIF profile for Data Descripition.
+
+Please note that there are no coded variables in the example - these would be represented using SKOS in CDIF.
+Also, the structure of the JSON-LD might change - the RDF content is correct, but there may be new rules for how this would look when integrated with a Schema.org wrapper in CDIF. This is under discussion as of November 2025.
--- hdf5Work/readNetCDF.py
+++ hdf5Work/readNetCDF.py
@@ -1,22 +1,28 @@
 import netCDF4 as nc
 import numpy as np
 
-dir = "C:/Users/smrTu/OneDrive/Documents/Workspace/Projects/CDIF-Dagstuhl/cdif/XrayAbsorbtion/"
-filename = '20231120_002_1mg_Murchison_Smithsonian.cdf'
-
+dir = "C:/Users/smrTu/OneDrive/Documents/GithubC/CDIF/integrationPublic/exampleMetadata/WDCC_NetCDF/"
+filename = "sftlf_fx_AWI-CM-1-1-MR_ssp370_r1i1p1f1_gn.nc"
+#dir = "C:/Users/smrTu/OneDrive/Documents/GithubC/CDIF/integrationPublic/ExampleData/"
+#dir = "C:/Users/smrTu/Downloads/"
+#filename = '20231120_002_1mg_Murchison_Smithsonian.cdf'
+#filename = "hur_Amon_UKESM1-0-LL_1pctCO2-cdr_r1i1p1f2_gn_255001-263912.nc"
 ds = nc.Dataset(dir + filename, 'r')
 
-print(ds.variables.keys)
+# print("variables: ", ds.variables)
+print("variables keys", ds.variables.keys)
 
 for dim in ds.dimensions.values():
- print(dim)
+ print("dimension: ", dim)
 
 print('...............')
 
 for var in ds.variables.values():
-    print(var)
+    print("variable: ", var)
     print('.....')
 
+print("###############################")
+print("keys and values in dataset")
 for akey in ds.__dict__.keys():
     if ds.__dict__[akey]:
         print(f'{akey}: {ds.__dict__[akey]}')
